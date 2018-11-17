c838d1ea0d47725550508fa5ab6d055d8a190566
Hunter Carlisle <huntingcarlisle@gmail.com>
2018-11-17 22:56:33 +0000

    automatically backed up by learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -10,9 +10,9 @@
 class CreateCostumes < ActiveRecord::Migration
   def change
     create_table :costumes do |t|
-      t.string :name,
-      t.integer :price,
-      t.string :size,
+      t.string :name
+      t.integer :price
+      t.string :size
       t.string :image_url
       t.timestamps
     end
